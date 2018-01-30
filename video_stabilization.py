01114fa805761a634ab00c28488b7ce880d2a13a
Thomas Reaney <thomas@reaney.net>
2018-01-30 13:22:16 +0000

    Converted script into a callable class

--- video_stabilization.py
+++ video_stabilization.py
@@ -4,142 +4,149 @@ import numpy as np
 import pandas as pd
 import tqdm
 import imutils
+import os
 
 
-# Required paths
-video_path = 'video1.mp4'
-output_dir = 'output'
-side_by_side = True
-max_width = 400
+class VideoStabilizer:
+    def __init__(self, video_in_path, out_path, out_file_name='output_video.avi', side_by_side=False, max_width=600):
+        # Initialize arguments
+        self.video_in_path = video_in_path
+        self.out_path = out_path
+        self.out_file_name = out_file_name
+        self.side_by_side = side_by_side
+        self.max_width = max_width
 
-# Set up video capture
-video = cv2.VideoCapture(video_path)
-n_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = int(video.get(cv2.CAP_PROP_FPS))
-print('[INFO]: Video loaded')
+        # Set up video capture
+        self.video = cv2.VideoCapture(self.video_in_path)
+        self.n_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = int(self.video.get(cv2.CAP_PROP_FPS))
 
-# Read 'prev', convert to greyscale and get dimensions
-_, prev = video.read()
-prev_g = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-(frame_h, frame_w) = prev_g.shape
+        # Read 'prev', convert to greyscale and get dimensions
+        _, self.prev = self.video.read()
+        self.prev_g = cv2.cvtColor(self.prev, cv2.COLOR_BGR2GRAY)
+        (self.frame_h, self.frame_w) = self.prev_g.shape
+        print('[INFO]: Setup completed')
 
-# -----------------------------------------------------------------
-print('[INFO]: Trajectory data calculations starting....')
-transform_data = []
+        # Calculate trajectory data
+        self.calculate_trajectory_data()
 
-for i in tqdm.tqdm(np.arange(n_frames)):
-    # Read frame
-    ok, curr = video.read()
+    def calculate_trajectory_data(self):
+        print('[INFO]: Trajectory data calculations starting....')
+        transform_data = []
 
-    if ok:
-        curr_g = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
+        for _ in tqdm.tqdm(np.arange(self.n_frames)):
+            # Read frame
+            ok, curr = self.video.read()
 
-        # Keypoint detection in 'prev_g'
-        prev_pts = cv2.goodFeaturesToTrack(prev_g, maxCorners=200, qualityLevel=0.01, minDistance=30.0, blockSize=3)
+            if ok:
+                # Convert to greyscale
+                curr_g = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
 
-        # Calculate optical flow (Lucas-Kanade Method)
-        curr_pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_g, curr_g, prev_pts, None)
+                # Keypoint detection in 'prev_g'
+                prev_pts = cv2.goodFeaturesToTrack(self.prev_g, maxCorners=200, qualityLevel=0.01, minDistance=30.0,
+                                                   blockSize=3)
 
-        curr_kpts, prev_kpts = [], []
+                # Calculate optical flow (Lucas-Kanade Method)
+                curr_pts, status, _ = cv2.calcOpticalFlowPyrLK(self.prev_g, curr_g, prev_pts, None)
+                curr_kpts, prev_kpts = [], []
 
-        # Save common keypoints found in 'prev' and 'curr'
-        for j, stat in enumerate(status):
+                # Save common keypoints found in 'prev' and 'curr'
+                for j, stat in enumerate(status):
+                    if stat == 1:
+                        # Save keypoints that appear in both
+                        prev_kpts.append(prev_pts[j])
+                        curr_kpts.append(curr_pts[j])
 
-            if stat == 1:
-                # Save keypoints that appear in both
-                prev_kpts.append(prev_pts[j])
-                curr_kpts.append(curr_pts[j])
+                # Estimate partial transform
+                curr_kpts, prev_kpts = np.array(curr_kpts), np.array(prev_kpts)
+                transform_new = cv2.estimateRigidTransform(prev_kpts, curr_kpts, False)
 
-        curr_kpts, prev_kpts = np.array(curr_pts), np.array(prev_pts)
+                if transform_new is not None:
+                    transform = transform_new
 
-        # Estimate partial transform
-        transform_new = cv2.estimateRigidTransform(prev_kpts, curr_kpts, False)
+                # x, y and rotational translations
+                dx = transform[0, 2]
+                dy = transform[1, 2]
+                da = np.arctan2(transform[1, 0], transform[0, 0])
 
-        if transform_new is not None:
-            transform = transform_new
+                # Store for saving to disk as table
+                transform_data.append([dx, dy, da])
 
-        # x, y and rotational translations
-        dx = transform[0, 2]
-        dy = transform[1, 2]
-        da = np.arctan2(transform[1, 0], transform[0, 0])
+                # Set current frame to previous frame for the next iteration
+                self.prev = curr[:]
+                self.prev_g = curr_g[:]
 
-        # Store for saving to disk as table
-        transform_data.append([dx, dy, da])
+        # Calculate the cumulative sum of all transforms for the trajectory
+        transform_data = np.array(transform_data)
+        trajectory = np.cumsum(transform_data, axis=0)
 
-        # Set current frame to previous frame for the next iteration
-        prev = curr[:]
-        prev_g = curr_g[:]
-# -----------------------------------------------------------------
+        # Calculate rolling mean to smooth trajectory, 'backfill' and save to CSV file
+        trajectory = pd.DataFrame(trajectory)
+        smoothed_trajectory = trajectory.rolling(window=30, center=False).mean()
+        smoothed_trajectory = smoothed_trajectory.fillna(method='bfill')
 
-# -----------------------------------------------------------------
-transform_data = np.array(transform_data)
+        # Remove 'trajectory', replace with 'smoothed_trajectory' and save to CSV file
+        new_transform_data = transform_data + (smoothed_trajectory - trajectory)
+        print('[INFO]: Trajectory data calculations finished')
 
-# Calculate the cumulative sum of all transforms for the trajectory
-trajectory = np.cumsum(transform_data, axis=0)
+        # Start actual video stabilization
+        self.video_stabilization(new_transform_data)
 
-# Calculate rolling mean to smooth trajectory, 'backfill' and save to CSV file
-trajectory = pd.DataFrame(trajectory)
-smoothed_trajectory = trajectory.rolling(window=30, center=False).mean()
-smoothed_trajectory = smoothed_trajectory.fillna(method='bfill')
-smoothed_trajectory.to_csv('{}/smoothed_trajectory.csv'.format(output_dir))
+    def video_stabilization(self, transform_data):
+        print('[INFO]: Actual video stabilization starting....')
+        # Initialize transformation matrix
+        t = np.zeros((2, 3))
+        transform_data = np.array(transform_data)
 
-# Remove 'trajectory', replace with 'smoothed_trajectory' and save to CSV file
-new_transform_data = transform_data + (smoothed_trajectory - trajectory)
-new_transform_data.to_csv('{}/new_transform_data.csv'.format(output_dir))
-print('[INFO]: Trajectory data calculations finished')
-# -----------------------------------------------------------------
+        # Setup video capture
+        cap = cv2.VideoCapture(self.video_in_path)
 
-# -----------------------------------------------------------------
-# Applying Video Stabilization
-# Initialize transformation matrix
-t = np.zeros((2, 3))
-new_transform_data = np.array(new_transform_data)
+        # Set output width and resize
+        w_write = min(self.frame_w, self.max_width)
+        h_write = imutils.resize(self.prev_g, width=w_write).shape[0]
 
-# Setup video capture
-cap = cv2.VideoCapture(video_path)
+        # Double output width if 'side_by_side' is 'True'
+        if self.side_by_side:
+            w_write = w_write * 2
 
-# Set output width and resize
-w_write = min(frame_w, max_width)
-h_write = imutils.resize(prev_g, width=w_write).shape[0]
+        # Setup video writer
+        video_out = cv2.VideoWriter(os.path.join(self.out_path, self.out_file_name),
+                                    cv2.VideoWriter_fourcc('P', 'I', 'M', '1'), self.fps, (w_write, h_write), True)
 
-# Double output width if 'side_by_side' is 'True'
-if side_by_side:
-    w_write = w_write * 2
+        for k in np.arange(self.n_frames-1):
+            # Read frame
+            _, curr = cap.read()
 
-# Setup video writer
-video_out = cv2.VideoWriter('{}/stabilized_output_video.avi'.format(output_dir),
-                            cv2.VideoWriter_fourcc('P', 'I', 'M', '1'), fps, (w_write, h_write), True)
+            # Read/build transformation matrix
+            t[0, 0] = np.cos(transform_data[k][2])
+            t[0, 1] = -np.sin(transform_data[k][2])
+            t[0, 2] = transform_data[k][0]
+            t[1, 0] = np.sin(transform_data[k][2])
+            t[1, 1] = np.cos(transform_data[k][2])
+            t[1, 2] = transform_data[k][1]
 
-for k in np.arange(n_frames-1):
-    # Read frame
-    _, curr = cap.read()
+            # Apply saved transform
+            curr_t = cv2.warpAffine(curr, t, (self.frame_w, self.frame_h))
 
-    # Read/build transformation matrix
-    t[0, 0] = np.cos(new_transform_data[k][2])
-    t[0, 1] = -np.sin(new_transform_data[k][2])
-    t[0, 2] = new_transform_data[k][0]
-    t[1, 0] = np.sin(new_transform_data[k][2])
-    t[1, 1] = np.cos(new_transform_data[k][2])
-    t[1, 2] = new_transform_data[k][1]
+            if self.side_by_side:
+                # Resize to 'max_width' if 'frame_w' > than 'max_width'
+                curr = imutils.resize(curr, width=min(self.frame_w, self.max_width))
+                curr_t = imutils.resize(curr_t, width=min(self.frame_w, self.max_width))
+                # combine arrays for side by side
+                frame_out = np.hstack((curr, curr_t))
+            else:
+                # Resize to 'max_width' if 'frame_w' > than 'max_width'
+                frame_out = imutils.resize(curr_t, width=min(self.frame_w, self.max_width))
 
-    # Apply saved transform
-    curr_t = cv2.warpAffine(curr, t, (frame_w, frame_h))
+            # Display frame
+            cv2.imshow('Output', frame_out)
+            cv2.waitKey(20)
 
-    if side_by_side:
-        # Resize to 'max_width' if 'frame_w' > than 'max_width'
-        curr = imutils.resize(curr, width=min(frame_w, max_width))
-        curr_t = imutils.resize(curr_t, width=min(frame_w, max_width))
-        # combine arrays for side by side
-        frame_out = np.hstack((curr, curr_t))
-    else:
-        # Resize to 'max_width' if 'frame_w' > than 'max_width'
-        frame_out = imutils.resize(curr_t, width=min(frame_w, max_width))
+            # Write frame to output video
+            video_out.write(frame_out)
 
-    # Display frame
-    cv2.imshow('Output', frame_out)
-    cv2.waitKey(20)
+        print('[INFO]: Actual video stabilization finished')
+        print('[INFO]: {} saved in {}'.format(self.out_file_name, self.out_path))
 
-    # Write frame to output video
-    video_out.write(frame_out)
 
-print('[INFO]: She done bai')
+VideoStabilizer('videos/video1.mp4', 'output', side_by_side=True)
